27fd324f428c2be63bb47b08d342b39f1702822f
deivisondev <deivisoneditordevideos@gmail.com>
2022-07-09 18:06:13 -0300

    Resolução do erro CORS

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    app.use(cors());
+    next();
+});
+
 app.use('/', require('./route/routes'));
 
 app.listen(3000);
